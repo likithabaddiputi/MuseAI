23153f103d7e1ab0c153bbe92357d67283a32351
Likitha Baddiputi <likithabaddiputi@gmail.com>
2025-11-06 11:21:13 +0530

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture("https://100.99.230.5:8080/video")
+
+while True:
+    success,img =  cap.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("gray",gray)
+    blur  = cv2.GaussianBlur(gray,(3,3),0)
+    cv2.imshow("edges",cv2.Canny(blur,50,50))
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
